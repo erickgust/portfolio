81eca8c272e777321fe96c29c948e3b22e684c39
Erick Gustavo <erick.stazs@gmail.com>
2022-10-17 00:14:43 -0300

    Add theme

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -2,5 +2,15 @@ import 'styled-components'
 
 declare module 'styled-components' {
   export interface DefaultTheme {
+    colors: {
+      greenLight: string
+      white: string
+      gray: {
+        200: string
+        400: string
+        main: string
+      }
+      dark: string
+    }
   }
 }
---
+++ src/resources/theme/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    greenLight: '#00DF5E',
+    white: '#F9F9F9',
+    gray: {
+      main: '#828282',
+      200: '#333333',
+      400: '#212121',
+    },
+    dark: '#171717',
+  },
+}
