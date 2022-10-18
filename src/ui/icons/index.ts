e4e1db8d6cf3cebe499f91cabc8baed0463e0eaa
Erick Gustavo <erick.stazs@gmail.com>
2022-10-18 16:43:41 -0300

    Add social icons

---
+++ src/ui/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as github } from './github.svg'
+export { default as mail } from './mail.svg'
+export { default as phone } from './phone.svg'
+export { default as smile } from './smile.svg'
