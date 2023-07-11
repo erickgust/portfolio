ce3454a81c5f2c1150de9579c80f175e213c06b9
Erick Gustavo <erick.stazs@gmail.com>
2023-07-11 16:18:09 -0300

    add skills icons

---
+++ src/ui/skills-icons/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as Css } from './css.svg'
+export { ReactComponent as Html } from './html.svg'
+export { ReactComponent as React } from './react.svg'
+export { ReactComponent as Tailwind } from './tailwind.svg'
+export { ReactComponent as Javascript } from './javascript.svg'
+export { ReactComponent as Typescript } from './typescript.svg'
