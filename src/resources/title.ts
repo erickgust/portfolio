e7dd9e5f1cd2c0dc9ef7e6b9330217fcab207fca
Erick Gustavo <erick.stazs@gmail.com>
2023-07-11 16:42:46 -0300

    style: add media query for Title component

--- src/resources/title.ts
+++ src/resources/title.ts
@@ -4,4 +4,8 @@ export const Title = styled.h2`
   font-size: 3.2rem;
   color: ${({ theme }) => theme.colors.white};
   text-align: center;
+
+  @media (max-width: 768px) {
+    font-size: 2.4rem;
+  }
 `
