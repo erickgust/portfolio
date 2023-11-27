a38cc116772fa252eb5409c78a0c653602c55878
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2023-11-27 10:26:14 -0300

    fix: anchor margin on titles (#3)
    * fix: header overlapping section titles

--- src/resources/title.ts
+++ src/resources/title.ts
@@ -4,6 +4,8 @@ export const Title = styled.h2`
   font-size: 3.2rem;
   color: ${({ theme }) => theme.colors.white};
   text-align: center;
+  scroll-margin-top: 8rem;
+  scroll-snap-margin-top: 8rem;
 
   @media (max-width: 768px) {
     font-size: 2.4rem;
